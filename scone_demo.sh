80902cb1f657d82f925eb171ce10cdf82bf5198f
Wesley Eddy <wesleyeddy@meta.com>
2025-11-02 18:17:11 +0000

    add IPv6 support to demo shell script

--- scone_demo.sh
+++ scone_demo.sh
@@ -48,9 +48,19 @@ CSP2_IP1=10.0.21.2
 CSP2_IP2=10.0.31.1
 CAP_IP=10.0.31.2
 
+USER_IPV6=2001:db8:11::1
+CSP1_IPV6_1=2001:db8:11::2
+CSP1_IPV6_2=2001:db8:21::1
+CSP2_IPV6_1=2001:db8:21::2
+CSP2_IPV6_2=2001:db8:31::1
+CAP_IPV6=2001:db8:31::2
+
 PLAIN_UDP_PORT=10000
 NON_SCONE_PORT=20000
 SCONE_PORT=60000
+PLAIN_UDP6_PORT=10001
+NON_SCONE6_PORT=20001
+SCONE6_PORT=60001
 
 # hq wants to use /tmp/logs.
 mkdir -p /tmp/logs
@@ -63,14 +73,22 @@ ip netns add $SCONE_USER_NS
 ip netns add $SCONE_CSP1_NS
 ip netns add $SCONE_CSP2_NS
 ip netns add $SCONE_CAP_NS
+
 ip netns exec $SCONE_USER_NS sysctl -q -w net.ipv4.ip_forward=1
 ip netns exec $SCONE_CSP1_NS sysctl -q -w net.ipv4.ip_forward=1
 ip netns exec $SCONE_CSP2_NS sysctl -q -w net.ipv4.ip_forward=1
 ip netns exec $SCONE_CAP_NS sysctl -q -w net.ipv4.ip_forward=1
+
+ip netns exec $SCONE_USER_NS sysctl -q -w net.ipv6.conf.all.forwarding=1
+ip netns exec $SCONE_CSP1_NS sysctl -q -w net.ipv6.conf.all.forwarding=1
+ip netns exec $SCONE_CSP2_NS sysctl -q -w net.ipv6.conf.all.forwarding=1
+ip netns exec $SCONE_CAP_NS sysctl -q -w net.ipv6.conf.all.forwarding=1
+
 echo "  Creating virtual links."
 ip -n $SCONE_USER_NS link add name $USER_CSP_LINK type veth peer netns $SCONE_CSP1_NS name $CSP_USER_LINK
 ip -n $SCONE_CSP1_NS link add name $CSP_CSP_1_LINK type veth peer netns $SCONE_CSP2_NS name $CSP_CSP_2_LINK
 ip -n $SCONE_CSP2_NS link add name $CSP_CAP_LINK type veth peer netns $SCONE_CAP_NS name $CAP_CSP_LINK
+
 echo "  Assigning IP addresses and bringing up interfaces."
 ip -n $SCONE_USER_NS address add $USER_IP/30 dev $USER_CSP_LINK
 ip -n $SCONE_CSP1_NS address add $CSP1_IP1/30 dev $CSP_USER_LINK
@@ -78,12 +96,21 @@ ip -n $SCONE_CSP1_NS address add $CSP1_IP2/30 dev $CSP_CSP_1_LINK
 ip -n $SCONE_CSP2_NS address add $CSP2_IP1/30 dev $CSP_CSP_2_LINK
 ip -n $SCONE_CSP2_NS address add $CSP2_IP2/30 dev $CSP_CAP_LINK
 ip -n $SCONE_CAP_NS address add $CAP_IP/30 dev $CAP_CSP_LINK
+
+ip -n $SCONE_USER_NS address add $USER_IPV6/64 dev $USER_CSP_LINK
+ip -n $SCONE_CSP1_NS address add $CSP1_IPV6_1/64 dev $CSP_USER_LINK
+ip -n $SCONE_CSP1_NS address add $CSP1_IPV6_2/64 dev $CSP_CSP_1_LINK
+ip -n $SCONE_CSP2_NS address add $CSP2_IPV6_1/64 dev $CSP_CSP_2_LINK
+ip -n $SCONE_CSP2_NS address add $CSP2_IPV6_2/64 dev $CSP_CAP_LINK
+ip -n $SCONE_CAP_NS address add $CAP_IPV6/64 dev $CAP_CSP_LINK
+
 ip -n $SCONE_USER_NS link set $USER_CSP_LINK up
 ip -n $SCONE_CSP1_NS link set $CSP_USER_LINK up
 ip -n $SCONE_CSP1_NS link set $CSP_CSP_1_LINK up
 ip -n $SCONE_CSP2_NS link set $CSP_CSP_2_LINK up
 ip -n $SCONE_CSP2_NS link set $CSP_CAP_LINK up
 ip -n $SCONE_CAP_NS link set $CAP_CSP_LINK up
+
 echo "  Setting routes."
 ip -n $SCONE_USER_NS route add $CAP_IP/32 via $CSP1_IP1
 ip -n $SCONE_CSP1_NS route add $CAP_IP/32 via $CSP2_IP1
@@ -92,6 +119,11 @@ ip -n $SCONE_CAP_NS route add $USER_IP/32 via $CSP2_IP2
 ip netns exec $SCONE_USER_NS ping -c 3 $CAP_IP
 #ip -n $SCONE_USER_NS stats set dev $USER_CSP_LINK l3_stats on
 
+ip -n $SCONE_USER_NS -6 route add $CAP_IPV6/128 via $CSP1_IPV6_1
+ip -n $SCONE_CSP1_NS -6 route add $CAP_IPV6/128 via $CSP2_IPV6_1
+ip -n $SCONE_CSP2_NS -6 route add $USER_IPV6/128 via $CSP1_IPV6_2
+ip -n $SCONE_CAP_NS -6 route add $USER_IPV6/128 via $CSP2_IPV6_2
+ip netns exec $SCONE_USER_NS ping6 -c 3 $CAP_IPV6
 
 # Attach eBPF programs.
 echo "=== Starting eBPF SCONE programs and packet capture."
@@ -121,6 +153,12 @@ ip netns exec $SCONE_CAP_NS nc -u -l $PLAIN_UDP_PORT & nc_pid=$!
 ip netns exec $SCONE_CAP_NS ./hq --mode=server --host=$CAP_IP --port=$NON_SCONE_PORT --logtostderr=false & hqs1_pid=$!
 ip netns exec $SCONE_CAP_NS ./hq --mode=server --host=$CAP_IP --port=$SCONE_PORT --logtostderr=false --h2port=7776 & hqs2_pid=$!
 # Note: h2port is set on the 2nd hq server above, so that it doesn't collide with the first.
+
+ip netns exec $SCONE_CAP_NS nc -u -l -6 $((PLAIN_UDP6_PORT + 1)) & nc6_pid=$!
+ip netns exec $SCONE_CAP_NS ./hq --mode=server --host=$CAP_IPV6 --port=$NON_SCONE6_PORT --logtostderr=false & hqs1_ipv6_pid=$!
+ip netns exec $SCONE_CAP_NS ./hq --mode=server --host=$CAP_IPV6 --port=$SCONE6_PORT --logtostderr=false --h2port=7777 & hqs2_ipv6_pid=$!
+
+
 echo "  Waiting 5 seconds."
 sleep 5
 
@@ -131,11 +169,17 @@ NC_DATA=/tmp/random_nc_data
 openssl rand -hex 48 >$NC_DATA
 #cat $NC_DATA | ip netns exec $SCONE_USER_NS nc -u $CAP_IP $PLAIN_UDP_PORT
 #ip netns exec $SCONE_USER_NS nc -u $CAP_IP $PLAIN_UDP_PORT
-echo "  Doing non-SCONE-enabled fetch."
+echo "  Doing non-SCONE-enabled fetch (IPv4)."
 ip netns exec $SCONE_USER_NS ./hq --mode=client --host=$CAP_IP --port=$NON_SCONE_PORT --path=/
 sleep 2
-echo "  Doing SCONE-enabled fetch."
+echo "  Doing SCONE-enabled fetch (IPv4)."
 ip netns exec $SCONE_USER_NS ./hq --mode=client --host=$CAP_IP --port=$SCONE_PORT --path=/
+sleep 2
+echo "  Doing non-SCONE-enabled fetch (IPv6)."
+ip netns exec $SCONE_USER_NS ./hq --mode=client --host=$CAP_IPV6 --port=$NON_SCONE6_PORT --path=/
+sleep 2
+echo "  Doing SCONE-enabled fetch (IPv6)."
+ip netns exec $SCONE_USER_NS ./hq --mode=client --host=$CAP_IPV6 --port=$SCONE6_PORT --path=/
 sleep 2
 
 # Stop and cleanup.
@@ -147,9 +191,12 @@ kill -9 $dump_pid4
 echo "  User network interface stats:"
 #ip netns exec $SCONE_USER_NS ip stats show dev $USER_CSP_LINK
 kill -9 $nc_pid
+kill -9 $nc6_pid
 kill -9 $hqs1_pid
+kill -9 $hqs1_ipv6_pid
 sleep 2
 kill -9 $hqs2_pid
+kill -9 $hqs2_ipv6_pid
 echo "  Stopping User<-CSP1 eBPF program."
 kill -2 $scone_pid1
 sleep 2
